ea52df30051d8423974cb264063dd5217c0b6e1d
Juan Loaiza <juanmazo1234@gmail.com>
2025-04-19 22:17:42 -0500

    Se aisla la funcion infoPid en el archivo infoPid.c

---
+++ funciones.h
@@ -0,0 +1,5 @@
+/* funciones.h */
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+void print_pid(char* argv[]);
+#endif
--- psinfo.c
+++ psinfo.c
@@ -4,61 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "funciones.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc < 2) {
         fprintf(stderr, "Uso: %s <PID>\n", argv[0]);
         return 1;
-    }
-
-    /* Construir ruta al archivo /proc/[pid]/status */
-    char path[64];
-    snprintf(path, sizeof(path), "/proc/%s/status", argv[1]);
-
-    FILE *f = fopen(path, "r");
-    if (!f) {
-        perror("Error al abrir /proc/[pid]/status");
+    }else if (argc == 2)
+    {
+        print_pid(argv);
+        return 0;
+    }else if (argc > 2)
+    {
+        fprintf(stderr, "Error: Demasiados argumentos.\n");
         return 1;
-    }
+    }    
 
-    /* Variables para almacenar la información requerida */
-    char line[256];
-    char name[64] = "";
-    char state[64] = "";
-    long vmSize = 0, vmExe = 0, vmData = 0, vmStk = 0;
-    long voluntary = 0, nonvoluntary = 0;
-
-    /* Leer línea a línea y extraer campos específicos */
-    while (fgets(line, sizeof(line), f)) {
-        if (strncmp(line, "Name:", 5) == 0) {
-            sscanf(line + 5, "%63s", name);
-        } else if (strncmp(line, "State:", 6) == 0) {
-            sscanf(line + 6, "%63s", state);
-        } else if (strncmp(line, "VmSize:", 7) == 0) {
-            sscanf(line + 7, "%ld", &vmSize);
-        } else if (strncmp(line, "VmExe:", 6) == 0) {
-            sscanf(line + 6, "%ld", &vmExe);
-        } else if (strncmp(line, "VmData:", 7) == 0) {
-            sscanf(line + 7, "%ld", &vmData);
-        } else if (strncmp(line, "VmStk:", 6) == 0) {
-            sscanf(line + 6, "%ld", &vmStk);
-        } else if (strncmp(line, "voluntary_ctxt_switches:", 24) == 0) {
-            sscanf(line + 24, "%ld", &voluntary);
-        } else if (strncmp(line, "nonvoluntary_ctxt_switches:", 27) == 0) {
-            sscanf(line + 27, "%ld", &nonvoluntary);
-        }
-    }
-    fclose(f);
-
-    /* Mostrar la información en pantalla */
-    printf("Nombre del proceso: %s\n", name);
-    printf("Estado: %s\n", state);
-    printf("Tamaño total de la imagen de memoria: %ld KB\n", vmSize);
-    printf("Tamaño de la memoria TEXT: %ld KB\n", vmExe);
-    printf("Tamaño de la memoria DATA: %ld KB\n", vmData);
-    printf("Tamaño de la memoria STACK: %ld KB\n", vmStk);
-    printf("Número de cambios de contexto (voluntarios - no voluntarios): %ld - %ld\n",
-           voluntary, nonvoluntary);
-
-    return 0;
 }
